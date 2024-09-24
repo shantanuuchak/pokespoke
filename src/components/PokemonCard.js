167a2524ed5ff0dc9edfc23fd4f34273024877b6
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2024-09-24 23:57:35 +0530

    show pokemon cards success!

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,20 +1,18 @@
 export default function (src, name, description, visit) {
   return `
-    <div class="col-sm-6 col-md-4 col-lg-3 mb-3 mb-sm-0 py-3 px-1">
-        <div class="card">
-        <img
-            src="${src}"
-            class="card-img-top"
-            alt="${name}"
-        />
-        <div class="card-body">
-            <h5 class="card-title">${name}</h5>
-            <p class="card-text">
-            ${description}
-            </p>
-            <a href="${visit}" class="btn btn-primary">Visit</a>
-        </div>
-        </div>
+    <div class="card">
+      <img
+          src="${src}"
+          class="card-img-top"
+          alt="${name}"
+      />
+      <div class="card-body">
+          <h5 class="card-title">${name}</h5>
+          <p class="card-text">
+          ${description}
+          </p>
+          <a href="${visit}" target="_blank" class="btn btn-primary">Visit</a>
+      </div>
     </div>
   `;
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,24 @@
-console.log("Hey there");
+import data from "./data.json";
+import PokemonCard from "../components/PokemonCard";
+
+// ====== DOM Selection ======
+const pokemonContainer = document.querySelector("[pokemon-container]");
+
+const pokemonCollection = data.map(({ name, image, description, link }) => {
+  return PokemonCard(image, name, description, link);
+});
+
+pokemonCollection.forEach((pokemonEl) => {
+  const div = document.createElement("div");
+  div.classList.add(
+    "col-sm-6",
+    "col-md-4",
+    "col-lg-3",
+    "mb-3",
+    "mb-sm-0",
+    "py-3",
+    "px-1"
+  );
+  div.innerHTML = pokemonEl;
+  pokemonContainer.appendChild(div);
+});
